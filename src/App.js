87593c9585148802a7629360e1e4d3d397781dce
erick odero <erickarthurs13@gmail.com>
2022-10-24 22:25:06 -0500

    Created a reusable button component and added Register and Log in buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Volunteer App</h1>
+        <Button>Register</Button>
+        <br />
+        <Button>Log in</Button>
       </header>
     </div>
   );
