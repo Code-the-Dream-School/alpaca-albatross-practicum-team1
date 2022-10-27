61d132312914d07167af1ff41191e868da2dfa0c
georgeowusu <gkojunior@gmail.com>
2022-10-27 18:07:51 -0400

    added prettier

--- src/components/PostIt.js
+++ src/components/PostIt.js
@@ -4,32 +4,30 @@ import PostItImg from '../images/postItImg.jpg';
 const PostIt = () => {
 	return (
 		<div className={styles.postContainter}>
-            <div className={styles.postArea}>
-                
+			<div className={styles.postArea}>
 				<div className={styles.post}>
 					<img src={PostItImg} alt='' />
 					<div>
 						<h2>Post 1</h2>
 						<p>Need help asap!</p>
 					</div>
-                </div>
-                
-                <div className={styles.post}>
+				</div>
+
+				<div className={styles.post}>
 					<img src={PostItImg} alt='' />
 					<div>
 						<h2>Post 2</h2>
 						<p>Need help asap!</p>
 					</div>
-                </div>
+				</div>
 
-                <div className={styles.post}>
+				<div className={styles.post}>
 					<img src={PostItImg} alt='' />
 					<div>
 						<h2>Post 3</h2>
 						<p>Need help asap!</p>
 					</div>
-                </div>
-
+				</div>
 			</div>
 		</div>
 	);
