4580c7dfaa705868d6d543586e73f666e78fd752
Dior.Shelton <dior.shelton@gmail.com>
2022-11-02 18:58:59 -0400

    Add logic so that login and register buttons navigate to their respective pages

--- src/modules/home/components/LoginButton.js
+++ src/modules/home/components/LoginButton.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import styles from '../styles/LoginButton.module.css'
+import { useNavigate } from 'react-router-dom'
+import { loginPath } from '../../login/routes/LoginRoute'
 
 export const LoginButton = () => {
+    const toLogin = useNavigate()
+
     return (
         <div>
-            <button className={styles.loginButton}>Log in</button>
+            <button
+                className={styles.loginButton}
+                onClick={() => toLogin(loginPath)}
+            >
+                Log in
+            </button>
         </div>
     )
 }
--- src/modules/home/components/RegisterButton.js
+++ src/modules/home/components/RegisterButton.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import styles from '../styles/RegisterButton.module.css'
+import { useNavigate } from 'react-router-dom'
+import { registrationPath } from '../../registration/routes/RegistrationRoute'
 
 export const RegisterButton = () => {
+    const toRegistration = useNavigate()
+
     return (
         <div>
-            <button className={styles.registerButton}>Register</button>
+            <button
+                className={styles.registerButton}
+                onClick={() => toRegistration(registrationPath)}
+            >
+                Register
+            </button>
         </div>
     )
 }
