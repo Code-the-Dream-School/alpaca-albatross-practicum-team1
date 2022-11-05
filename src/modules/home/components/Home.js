d4178a011a45677b043af8a85131e0c286543e1e
georgeowusu <gkojunior@gmail.com>
2022-11-04 23:45:05 -0400

    retreiving post from endpoint

--- src/modules/home/components/Home.js
+++ src/modules/home/components/Home.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Postings } from './Postings'
 import { LoginButton } from './LoginButton'
 import { RegisterButton } from './RegisterButton'
 import styles from '../styles/Home.module.css'
+import axios from 'axios'
 
 export const Home = () => {
+    const [data, getData] = useState([])
+
+    useEffect(() => {
+        axios
+            .get(
+                'https://dpx3nob3b4anj5ujjhfudnsspu0ubhst.lambda-url.ap-southeast-2.on.aws/'
+            )
+            .then(res => {
+                console.log("Getting from API ENDPOINT", res.data)
+                getData(res.data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
+
+
     return (
         <div>
             <div className={styles.homeHeader}>
@@ -15,7 +31,7 @@ export const Home = () => {
                     <RegisterButton />
                 </div>
             </div>
-            <Postings />
+            <Postings data={data}/>
         </div>
     )
 }
