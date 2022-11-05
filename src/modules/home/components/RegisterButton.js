7b906a6e75c4eb13f21d4a3d6c6f35f90d98a934
Dior.Shelton <dior.shelton@gmail.com>
2022-11-04 22:28:41 -0400

    Correct syntax

--- src/modules/home/components/RegisterButton.js
+++ src/modules/home/components/RegisterButton.js
@@ -4,14 +4,15 @@ import { useNavigate } from 'react-router-dom'
 import { registrationPath } from '../../registration/routes/RegistrationRoute'
 
 export const RegisterButton = () => {
-    
     const navigate = useNavigate()
 
     const onClick = () => navigate(registrationPath)
 
     return (
         <div>
-            <button className={styles.registerButton} onClick={onClick}>Register</button>
+            <button className={styles.registerButton} onClick={onClick}>
+                Register
+            </button>
         </div>
     )
 }
