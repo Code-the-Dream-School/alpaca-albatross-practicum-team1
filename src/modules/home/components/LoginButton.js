ee6b036489af725af56ae591b3256e92d53d4527
erick odero <erickarthurs13@gmail.com>
2022-11-06 15:05:08 -0600

    added call to login

--- src/modules/home/components/LoginButton.js
+++ src/modules/home/components/LoginButton.js
@@ -1,54 +1,10 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
-import ClipLoader from 'react-spinners/ClipLoader'
+import React from 'react'
 import styles from '../styles/LoginButton.module.css'
-import { loginPath } from '../../login/routes/LoginRoute'
 
 export const LoginButton = () => {
-    const [isPending, setIsPending] = useState(false)
-    const [error, setError] = useState(null)
-    const navigate = useNavigate()
-
-    const handleClick = async () => {
-        try {
-            const res = await axios.get(
-                'https://zj7ipg4ixun5mj77y6g6t7qkce0omtjb.lambda-url.ap-southeast-2.on.aws/'
-            )
-            setIsPending(true)
-
-            if (res.data.isLoginSuccess) {
-                navigate(loginPath)
-                setIsPending(false)
-            }
-        } catch (err) {
-            if (err.name === 'AxiosError') {
-                console.log(err.message)
-            } else {
-                setIsPending(false)
-                setError('Login unsuccessful')
-            }
-        }
-    }
-
     return (
         <div>
-            {error && <span>{error}</span>}
-            <button
-                className={styles.loginButton}
-                onClick={handleClick}
-                disabled={isPending}
-            >
-                {isPending && (
-                    <ClipLoader
-                        color={'#ffff'}
-                        size={20}
-                        aria-label="Loading Spinner"
-                    />
-                )}
-                {isPending && <span>Loading...</span>}
-                {!isPending && <span>Log in</span>}
-            </button>
+            <button className={styles.loginButton}>Log in</button>
         </div>
     )
 }
--- src/modules/login/components/Login.js
+++ src/modules/login/components/Login.js
@@ -1,7 +1,14 @@
 import React, { useState } from 'react'
 import styles from '../styles/Login.module.css'
+import ClipLoader from 'react-spinners/ClipLoader'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { homePath } from '../../home/routes/HomeRoute'
 
 export const Login = () => {
+    const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
+    const navigate = useNavigate()
     const [loginData, SetLoginData] = useState({
         email: '',
         password: '',
@@ -19,15 +26,24 @@ export const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        const res = await fetch('http://localhost:3001/login', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-            },
-            body: JSON.stringify(),
-        }).then((res) => res.json())
-        SetLoginData(res)
-        console.log(loginData)
+        try {
+            const res = await axios.get(
+                'https://zj7ipg4ixun5mj77y6g6t7qkce0omtjb.lambda-url.ap-southeast-2.on.aws/'
+            )
+            setIsPending(true)
+
+            if (res.data.isLoginSuccess) {
+                navigate(homePath)
+                setIsPending(false)
+            }
+        } catch (err) {
+            if (err.name === 'AxiosError') {
+                console.log(err.message)
+            } else {
+                setError('Login unsuccessful')
+            }
+        }
+        SetLoginData(loginData)
     }
 
     return (
@@ -51,7 +67,22 @@ export const Login = () => {
                     required
                 />
 
-                <button className={styles.loginButton}>Log in</button>
+                {error && <span>{error}</span>}
+                <button
+                    className={styles.loginButton}
+                    onClick={handleSubmit}
+                    disabled={isPending}
+                >
+                    {isPending && (
+                        <ClipLoader
+                            color={'#ffff'}
+                            size={20}
+                            aria-label="Loading Spinner"
+                        />
+                    )}
+                    {isPending && <span>Loading...</span>}
+                    {!isPending && <span>Log in</span>}
+                </button>
             </form>
 
             {/*Insert ForgotPassword route here*/}
