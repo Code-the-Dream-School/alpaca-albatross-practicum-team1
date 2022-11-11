dc637fadbbbb86ae1fc574133d105a672119ecb5
georgeowusu <gkojunior@gmail.com>
2022-11-10 19:26:34 -0500

    added lint and updated file

--- src/modules/home/components/RegisterButton.js
+++ src/modules/home/components/RegisterButton.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import styles from '../styles/RegisterButton.module.css'
+import { useNavigate } from 'react-router-dom'
+import { registrationPath } from '../../registration/routes/RegistrationRoute'
 
 export const RegisterButton = () => {
+    const navigate = useNavigate()
+
+    const onClick = () => navigate(registrationPath)
+
     return (
         <div>
-            <button className={styles.registerButton}>Register</button>
+            <button className={styles.registerButton} onClick={onClick}>
+                Register
+            </button>
         </div>
     )
 }
+
