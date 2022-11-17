c779618e2bb65bbfbe2c337b2706b87ab8103cff
georgeowusu <gkojunior@gmail.com>
2022-11-17 18:45:07 -0500

    Updated form and fetch request

--- src/modules/add-post/components/AddPost.js
+++ src/modules/add-post/components/AddPost.js
@@ -1,31 +1,27 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import axios from 'axios'
 import styles from '../styles/AddPost.module.css'
 
 export const AddPost = () => {
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
 
         const formData = new FormData(e.currentTarget)
         const data = Object.fromEntries(formData)
+        const requestData = {
+            ...data,
+            // TODO and username once react useContext implemented
+            username: 'Team1'
+        }
 
-        console.log(data)
-
-        useEffect(() => {
-            axios
-                .post(
-                    'https://dpx3nob3b4anj5ujjhfudnsspu0ubhst.lambda-url.ap-southeast-2.on.aws/',
-                    {
-                        method: 'POST',
-                        headers: { 'Content-Type': 'application/json' },
-                        body: JSON.stringify(data)
-                    }
-                )
-                .then(() => {
-                    console.log('New Post Added')
-                })
-                .catch((err) => console.log(err))
-        }, [])
+        axios
+            .post('https://localhost:3001/post/createPost', {
+                ...requestData
+            })
+            .then(() => {
+                console.log('New Post Added')
+            })
+            .catch((err) => console.log(err))
     }
 
     return (
@@ -36,8 +32,6 @@ export const AddPost = () => {
                 <input type="text" required name="title" />
                 <label>Post Message:</label>
                 <textarea type="text" required name="message" />
-                <label>Email:</label>
-                <input type="text" required name="email" />
                 <button>ADD POST</button>
             </form>
         </div>
