c06ef6b6895ccc5cbe7a003ee407cfdee4806324
georgeowusu <gkojunior@gmail.com>
2022-12-14 07:05:24 -0500

    updated paths in file

--- src/App.js
+++ src/App.js
@@ -7,17 +7,22 @@ import { Login } from './modules/login/components/Login'
 import { MyPosts } from './modules/myposts/components/MyPosts'
 import { Registration } from './modules/registration/components/Registration'
 import { NavigationLinks } from './modules/home/components/NavigationLinks'
+import { homePath } from './modules/home/routes/HomeRoute'
+import { addPostPath } from './modules/add-post/routes/AddPostRoute'
+import { loginPath } from './modules/login/routes/LoginRoute'
+import { registrationPath } from './modules/registration/routes/RegistrationRoute'
+import { myPostsPath } from './modules/myposts/routes/MyPostsRoute'
 
 function App() {
     return (
         <>
             <NavigationLinks />
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/add-post" element={<AddPost />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/registration" element={<Registration />} />
-                <Route path="/myposts" element={<MyPosts />} />
+                <Route path={homePath} element={<Home />} />
+                <Route path={addPostPath} element={<AddPost />} />
+                <Route path={loginPath} element={<Login />} />
+                <Route path={registrationPath} element={<Registration />} />
+                <Route path={myPostsPath} element={<MyPosts />} />
             </Routes>
         </>
     )
