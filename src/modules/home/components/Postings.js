fb16b9c704d3f2d77420383ff044971eff5d299f
Amy (she/her) <98506565+SandovalAmy@users.noreply.github.com>
2022-12-18 20:31:34 -0800

    Committed on new branch (#87)
    Co-authored-by: Leigh-K <99621803+Leigh-K@users.noreply.github.com>

--- src/modules/home/components/Postings.js
+++ src/modules/home/components/Postings.js
@@ -2,38 +2,53 @@ import React, { useEffect, useState } from 'react'
 import { Post } from './Post'
 import axios from 'axios'
 import styles from '../styles/Postings.module.css'
+import { LoadingSpinner } from '../../common/components/LoadingSpinner'
 
 export const Postings = () => {
     const [posts, setPosts] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
-        axios
-            // eslint-disable-next-line no-undef
-            .get(`${process.env.REACT_APP_SERVICE_ENDPOINT}/post/getPosts`)
-            .then((res) => {
-                setPosts(res.data.posts)
-            })
-            .catch((err) => console.log(err))
-    }, [])
-
-    if (!posts || !posts.length) {
-        return (
-            <div className={styles.posting}>No posts yet, check back later</div>
-        )
-    }
+        const getPosts = async () => {
+            setIsLoading(true)
+            try {
+                const response = await axios.get(
+                    // eslint-disable-next-line no-undef
+                    `${process.env.REACT_APP_SERVICE_ENDPOINT}/post/getPosts`
+                )
+                if (response.data.posts) {
+                    setPosts(response.data.posts)
+                    setIsLoading(false)
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getPosts()
+    }, [setPosts])
 
     return (
-        <div className={styles.posting}>
-            {posts.map((data, index) => {
-                return (
-                    <Post
-                        key={index}
-                        title={data.title}
-                        description={data.message}
-                        id={data._id}
-                    />
-                )
-            })}
-        </div>
+        <>
+            <div className={styles.posting}>
+                {posts.map((data, index) => {
+                    return (
+                        <Post
+                            key={index}
+                            title={data.title}
+                            description={data.message}
+                            id={data._id}
+                        />
+                    )
+                })}
+            </div>
+
+            {isLoading && (
+                <div className={styles.spinnerContainer}>
+                    <div className={styles.spinner}>
+                        <LoadingSpinner /> <h3>Posts are loading...</h3>
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
